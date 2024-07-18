637790a8de93ac9265820eb124d7d3737f51db09
AnaOgata <162324639+AnaOgata@users.noreply.github.com>
2024-07-18 15:01:22 -0300

    Create SomaSimples.c

---
+++ SomaSimples.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+    int A, B;
+    int soma = 0;
+    
+    scanf("%d", &A);
+    scanf("%d", &B);
+    
+    soma = A + B;
+    
+    printf("SOMA = %d\n", soma);
+    
+    return 0;
+}
